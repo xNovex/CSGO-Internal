6709ae73ee0821187292899f49ab0eef90459d32
xNovex <93387346+xNovex@users.noreply.github.com>
2026-05-29 02:28:55 +0200

    Add files via upload

--- client_dll.hpp
+++ client_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-22 23:12:41.094171500 UTC
+// 2026-05-29 00:27:53.427548700 UTC
 
 #pragma once
 
--- offsets.hpp
+++ offsets.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-22 23:12:41.094171500 UTC
+// 2026-05-29 00:27:53.427548700 UTC
 
 #pragma once
 
@@ -10,24 +10,24 @@ namespace cs2_dumper {
     namespace offsets {
         // Module: client.dll
         namespace client_dll {
-            constexpr std::ptrdiff_t dwAutoAccept = 0x23544E0;
-            constexpr std::ptrdiff_t dwCSGOInput = 0x2353190;
-            constexpr std::ptrdiff_t dwEntityList = 0x24E44E0;
-            constexpr std::ptrdiff_t dwGameEntitySystem = 0x24E44E0;
+            constexpr std::ptrdiff_t dwAutoAccept = 0x2355580;
+            constexpr std::ptrdiff_t dwCSGOInput = 0x2354230;
+            constexpr std::ptrdiff_t dwEntityList = 0x24E5590;
+            constexpr std::ptrdiff_t dwGameEntitySystem = 0x24E5590;
             constexpr std::ptrdiff_t dwGameEntitySystem_highestEntityIndex = 0x2090;
-            constexpr std::ptrdiff_t dwGameRules = 0x233E258;
-            constexpr std::ptrdiff_t dwGlobalVars = 0x205E6D0;
-            constexpr std::ptrdiff_t dwGlowManager = 0x233B050;
-            constexpr std::ptrdiff_t dwLocalPlayerController = 0x231D830;
-            constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x2069800;
-            constexpr std::ptrdiff_t dwPlantedC4 = 0x234CF08;
-            constexpr std::ptrdiff_t dwPrediction = 0x2069710;
-            constexpr std::ptrdiff_t dwSensitivity = 0x233BB68;
+            constexpr std::ptrdiff_t dwGameRules = 0x233F158;
+            constexpr std::ptrdiff_t dwGlobalVars = 0x205F6D0;
+            constexpr std::ptrdiff_t dwGlowManager = 0x233BF50;
+            constexpr std::ptrdiff_t dwLocalPlayerController = 0x231E700;
+            constexpr std::ptrdiff_t dwLocalPlayerPawn = 0x233F698;
+            constexpr std::ptrdiff_t dwPlantedC4 = 0x234DF88;
+            constexpr std::ptrdiff_t dwPrediction = 0x233F5A0;
+            constexpr std::ptrdiff_t dwSensitivity = 0x233CA68;
             constexpr std::ptrdiff_t dwSensitivity_sensitivity = 0x58;
-            constexpr std::ptrdiff_t dwViewAngles = 0x2353818;
-            constexpr std::ptrdiff_t dwViewMatrix = 0x2343AB0;
-            constexpr std::ptrdiff_t dwViewRender = 0x2343E58;
-            constexpr std::ptrdiff_t dwWeaponC4 = 0x22BBE30;
+            constexpr std::ptrdiff_t dwViewAngles = 0x23548B8;
+            constexpr std::ptrdiff_t dwViewMatrix = 0x2344B30;
+            constexpr std::ptrdiff_t dwViewRender = 0x2344ED8;
+            constexpr std::ptrdiff_t dwWeaponC4 = 0x22BCD00;
         }
         // Module: engine2.dll
         namespace engine2_dll {
